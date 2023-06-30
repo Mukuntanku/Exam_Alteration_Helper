d7187d39e75d969d0de908f50312dbe70dea14cf
Mukuntan K U <mukuntanku23@gmail.com>
2023-06-30 20:44:05 +0530

    updated the inbound rule for the express

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ dotenv.config();
 const app = express();
 app.use(cors(
     {
-        origin: ["http://localhost:3000"],
+        // origin: ["http://localhost:3000"],
         methods: ["POST", "GET", "PUT"],
         credentials: true
     }
