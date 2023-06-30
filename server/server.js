68e62d3608b39f0ae01f87b035d645d5db58a729
Mukuntan K U <mukuntanku23@gmail.com>
2023-06-30 23:21:59 +0530

    ....

--- server/server.js
+++ server/server.js
@@ -17,13 +17,14 @@ import { error } from 'console'
 dotenv.config();
 
 const app = express();
-app.use(cors(
-    {
-        origin: ["*"],
-        methods: ["POST", "GET", "PUT"],
-        credentials: true
-    }
-));
+app.use(cors);
+// app.use(cors(
+//     {
+//         origin: ["*"],
+//         methods: ["POST", "GET", "PUT"],
+//         credentials: true
+//     }
+// ));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.static('public'));
